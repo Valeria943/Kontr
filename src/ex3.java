898d5ccbac120862bba251ea32cfc56b14a95b4c
Valeria943 <82813302+Valeria943@users.noreply.github.com>
2022-05-24 16:43:35 +0900

    Initial commit

---
+++ src/ex3.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class ex3 {
+    public static void main(String[] args) {
+        System.out.println("Введите последовательность целых чисел, оканчивающихся 0:");
+        Scanner sc = new Scanner(System.in);
+        int curr = 0;
+        int summ = 0;
+        while ((curr = sc.nextInt()) != 0) {
+            summ += curr;
+        }
+        System.out.println(String.format("Сумма последовательности %d", summ));
+    }
+}
